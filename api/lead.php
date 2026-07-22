007eb70c8fec1c4f4b9c6a96d0a6f7e2913d76cb
fhranco <patagoniacoach@gmail.com>
2026-07-22 02:17:10 -0300

    style: rewrite client auto-responder to first-person singular

--- api/lead.php
+++ api/lead.php
@@ -106,8 +106,8 @@ $cuerpo_cliente = "
         <img src='https://boostpatagonia.com/boostpatagonia.png' alt='BoostPatagonia' class='logo'>
         <h1>¡Postulación Recibida!</h1>
         <p>Hola <span class='highlight'>{$nombre}</span>,</p>
-        <p>Hemos recibido correctamente tu postulación a nuestra convocatoria de la temporada 2026–2027.</p>
-        <p>Nuestro equipo de consultores senior está evaluando la información ingresada. Nos pondremos en contacto contigo a la brevedad vía WhatsApp para los siguientes pasos.</p>
+        <p>He recibido correctamente tu postulación a mi convocatoria de la temporada 2026–2027.</p>
+        <p>Estoy revisando personalmente los datos que me ingresaste. Me pondré en contacto contigo a la brevedad vía WhatsApp para ver los siguientes pasos.</p>
         <div class='btn-container'>
             <a href='https://boostpatagonia.com' class='btn'>Visitar Sitio Web</a>
         </div>
